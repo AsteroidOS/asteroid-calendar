ff2118d0b16041e9e743156085a95e714cb704aa
Paulo de Almeida <paulodealmeida@gmail.com>
2016-12-08 17:06:51 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ asteroid-calendar.pt_BR.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>EventDialog</name>
+    <message>
+        <location filename="EventDialog.qml" line="31"/>
+        <source>New Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="EventDialog.qml" line="31"/>
+        <source>Edit Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="EventDialog.qml" line="117"/>
+        <source>Alpha version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="EventDialog.qml" line="126"/>
+        <source>Some features are not developed yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MonthSelector</name>
+    <message>
+        <location filename="MonthSelector.qml" line="28"/>
+        <source>Select a date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="229"/>
+        <source>%1 Events on %2 %3/%4/%5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
