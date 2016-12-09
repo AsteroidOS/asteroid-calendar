5a8106ee18cb210662eefb0c10083cc2303bb0a4
snowmaan <gqbigpaps@gmail.com>
2016-12-09 08:53:28 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (6 of 6 strings)

--- asteroid-calendar.ko.ts
+++ asteroid-calendar.ko.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="EventDialog.qml" line="31"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>새 이벤트</translation>
     </message>
     <message>
         <location filename="EventDialog.qml" line="31"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>이벤트 수정</translation>
     </message>
     <message>
         <location filename="EventDialog.qml" line="117"/>
         <source>Alpha version</source>
-        <translation type="unfinished"></translation>
+        <translation>알파 버전</translation>
     </message>
     <message>
         <location filename="EventDialog.qml" line="126"/>
         <source>Some features are not developed yet</source>
-        <translation type="unfinished"></translation>
+        <translation>기능들이 아직 개발 중입니다</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
     <message>
         <location filename="MonthSelector.qml" line="28"/>
         <source>Select a date:</source>
-        <translation type="unfinished"></translation>
+        <translation>날짜 지정:</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="main.qml" line="229"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation type="unfinished"></translation>
+        <translation>%2 %3/%4/%5에 이벤트가 %1개 있음</translation>
     </message>
 </context>
 </TS>
