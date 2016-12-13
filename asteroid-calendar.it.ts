2d5f5cc44960e4ef6d884d4aff6972fcaf32465b
dr_gogeta86 <dr_gogeta86@messinalug.org>
2016-12-13 08:49:04 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (6 of 6 strings)

--- asteroid-calendar.it.ts
+++ asteroid-calendar.it.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="EventDialog.qml" line="31"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuovo Evento</translation>
     </message>
     <message>
         <location filename="EventDialog.qml" line="31"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifica Evento</translation>
     </message>
     <message>
         <location filename="EventDialog.qml" line="117"/>
         <source>Alpha version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versione Alpha</translation>
     </message>
     <message>
         <location filename="EventDialog.qml" line="126"/>
         <source>Some features are not developed yet</source>
-        <translation type="unfinished"></translation>
+        <translation>Alcune funzionalità non sono ancora implementate</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
     <message>
         <location filename="MonthSelector.qml" line="28"/>
         <source>Select a date:</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleziona una data:</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="main.qml" line="229"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Eventi il %2 %3/%4/%5</translation>
     </message>
 </context>
 </TS>
