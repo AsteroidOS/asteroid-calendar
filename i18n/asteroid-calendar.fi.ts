00cb8d9290d1c9ed309385c6f4be9b0ac1fb1442
Severi Hofslagare <zevez90@gmail.com>
2017-03-28 20:25:21 +0000

    Added translation using Weblate (Finnish)

---
+++ i18n/asteroid-calendar.fi.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>EventDialog</name>
+    <message>
+        <location filename="../EventDialog.qml" line="36"/>
+        <source>New Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EventDialog.qml" line="36"/>
+        <source>Edit Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EventDialog.qml" line="122"/>
+        <source>Alpha version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EventDialog.qml" line="131"/>
+        <source>Some features are not developed yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MonthSelector</name>
+    <message>
+        <location filename="../MonthSelector.qml" line="28"/>
+        <source>Select a date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-calendar.desktop.h" line="5"/>
+        <source>Agenda</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="237"/>
+        <source>%1 Events on %2 %3/%4/%5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
