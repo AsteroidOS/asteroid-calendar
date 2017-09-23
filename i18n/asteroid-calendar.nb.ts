7a5f61b6aad433ebf572ac0a9e1992750ae17b0a
Florent Revest <revestflo@gmail.com>
2017-09-23 07:58:57 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ i18n/asteroid-calendar.nb.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb">
+<context>
+    <name>EventDialog</name>
+    <message>
+        <location filename="../EventDialog.qml" line="36"/>
+        <source>New Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EventDialog.qml" line="36"/>
+        <source>Edit Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EventDialog.qml" line="115"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EventDialog.qml" line="157"/>
+        <source>Untitled event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alpha version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Some features are not developed yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MonthSelector</name>
+    <message>
+        <location filename="../MonthSelector.qml" line="28"/>
+        <source>Select a date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-calendar.desktop.h" line="5"/>
+        <source>Agenda</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="239"/>
+        <source>%1 Events on %2 %3/%4/%5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
