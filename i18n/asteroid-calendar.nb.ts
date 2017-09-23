368040411decd886b664e49c0a8ced1b65dc3370
Allan Nordhøy <epost@anotheragency.no>
2017-09-23 08:13:21 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (9 of 9 strings)

--- i18n/asteroid-calendar.nb.ts
+++ i18n/asteroid-calendar.nb.ts
@@ -6,30 +6,30 @@
     <message>
         <location filename="../EventDialog.qml" line="36"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Ny hendelse</translation>
     </message>
     <message>
         <location filename="../EventDialog.qml" line="36"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Endre hendelse</translation>
     </message>
     <message>
         <location filename="../EventDialog.qml" line="115"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Tittel</translation>
     </message>
     <message>
         <location filename="../EventDialog.qml" line="157"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>Tittel uten navn</translation>
     </message>
     <message>
         <source>Alpha version</source>
-        <translation type="unfinished"></translation>
+        <translation>Alfaversjon</translation>
     </message>
     <message>
         <source>Some features are not developed yet</source>
-        <translation type="unfinished"></translation>
+        <translation>Noen funksjoner er ikke utviklet enda</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Select a date:</source>
-        <translation type="unfinished"></translation>
+        <translation>Velg dato:</translation>
     </message>
 </context>
 <context>
@@ -45,7 +45,7 @@
     <message>
         <location filename="asteroid-calendar.desktop.h" line="5"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Gjøremål</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,7 @@
     <message>
         <location filename="../main.qml" line="239"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Hendelser den</translation>
     </message>
 </context>
 </TS>
