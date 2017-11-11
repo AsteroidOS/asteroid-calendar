657625c7e58410c78b1e592baa64dd7c0ec4391d
sam bowlby <sam@chven.us>
2017-11-11 15:42:03 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (9 of 9 strings)

--- i18n/asteroid-calendar.ka.ts
+++ i18n/asteroid-calendar.ka.ts
@@ -6,30 +6,30 @@
     <message>
         <location filename="../EventDialog.qml" line="36"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>ახალი ღონისძიება</translation>
     </message>
     <message>
         <location filename="../EventDialog.qml" line="36"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>ღონისძიების რედაქტირება</translation>
     </message>
     <message>
         <location filename="../EventDialog.qml" line="115"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>სათაური</translation>
     </message>
     <message>
         <location filename="../EventDialog.qml" line="157"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>უსათაურო ღონისძიება</translation>
     </message>
     <message>
         <source>Alpha version</source>
-        <translation></translation>
+        <translation>ალფა ვერსია</translation>
     </message>
     <message>
         <source>Some features are not developed yet</source>
-        <translation></translation>
+        <translation>ზოგიერთი ფუნქცია ჯერ კიდევ არ არის განვითარებული</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Select a date:</source>
-        <translation></translation>
+        <translation>აირჩიეთ თარიღი:</translation>
     </message>
 </context>
 <context>
@@ -45,7 +45,7 @@
     <message>
         <location filename="asteroid-calendar.desktop.h" line="5"/>
         <source>Agenda</source>
-        <translation></translation>
+        <translation>დღის წესრიგი</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,7 @@
     <message>
         <location filename="../main.qml" line="239"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%1 ღონისძიებები %2-ს %3/%4/%5</translation>
     </message>
 </context>
 </TS>
