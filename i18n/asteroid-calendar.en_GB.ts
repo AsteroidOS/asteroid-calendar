12d861295ea35e29c3b1f52a4e1b99734616f533
Florent Revest <revestflo@gmail.com>
2017-11-23 22:27:51 +0100

    Add a default english translation

---
+++ i18n/asteroid-calendar.en_GB.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name></name>
+    <message id="id-new-event">
+        <location filename="../EventDialog.qml" line="43"/>
+        <source>New Event</source>
+        <translation>New Event</translation>
+    </message>
+    <message id="id-edit-event">
+        <location filename="../EventDialog.qml" line="45"/>
+        <source>Edit Event</source>
+        <translation>Edit Event</translation>
+    </message>
+    <message id="id-title-field">
+        <location filename="../EventDialog.qml" line="97"/>
+        <source>Title</source>
+        <translation>Title</translation>
+    </message>
+    <message id="id-untitled-event">
+        <location filename="../EventDialog.qml" line="135"/>
+        <source>Untitled event</source>
+        <translation>Untitled event</translation>
+    </message>
+    <message id="id-date-selection">
+        <location filename="../MonthSelector.qml" line="28"/>
+        <source>Select a date:</source>
+        <translation>Select a date:</translation>
+    </message>
+    <message id="id-events-recap">
+        <location filename="../main.qml" line="238"/>
+        <source>%1 Events on %2 %3/%4/%5</source>
+        <translation>%1 Events on %2 %3/%4/%5</translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-calendar.desktop.h" line="6"/>
+        <source>Agenda</source>
+        <translation>Agenda</translation>
+    </message>
+</context>
+</TS>
