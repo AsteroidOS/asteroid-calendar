3310c285b68b8615a4ef0d620ec4359e9d689a39
Yaron Shahrabani <sh.yaron@gmail.com>
2017-11-25 13:18:08 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.he.ts
+++ i18n/asteroid-calendar.he.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>אירוע חדש</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>שינוי אירוע</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="97"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>כותרת</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="135"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>אירוע ללא שם</translation>
     </message>
     <message id="id-date-selection">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Select a date:</source>
-        <translation></translation>
+        <translation>בחירת תאריך:</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%1 אירועים ב־%2 %3/%4/%5</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation></translation>
+        <translation>סדר יום</translation>
     </message>
 </context>
 </TS>
