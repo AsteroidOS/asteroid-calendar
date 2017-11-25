0e515824188ecacda213d9deda77e22e891e52c8
Yaron Shahrabani <sh.yaron@gmail.com>
2017-11-25 13:16:56 +0000

    Added translation using Weblate (Hebrew)

---
+++ i18n/asteroid-calendar.he.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he">
+<context>
+    <name></name>
+    <message id="id-new-event">
+        <location filename="../EventDialog.qml" line="43"/>
+        <source>New Event</source>
+        <translation></translation>
+    </message>
+    <message id="id-edit-event">
+        <location filename="../EventDialog.qml" line="45"/>
+        <source>Edit Event</source>
+        <translation></translation>
+    </message>
+    <message id="id-title-field">
+        <location filename="../EventDialog.qml" line="97"/>
+        <source>Title</source>
+        <translation></translation>
+    </message>
+    <message id="id-untitled-event">
+        <location filename="../EventDialog.qml" line="135"/>
+        <source>Untitled event</source>
+        <translation></translation>
+    </message>
+    <message id="id-date-selection">
+        <location filename="../MonthSelector.qml" line="28"/>
+        <source>Select a date:</source>
+        <translation></translation>
+    </message>
+    <message id="id-events-recap">
+        <location filename="../main.qml" line="238"/>
+        <source>%1 Events on %2 %3/%4/%5</source>
+        <translation></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-calendar.desktop.h" line="6"/>
+        <source>Agenda</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
