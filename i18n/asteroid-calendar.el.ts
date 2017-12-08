5fe43edfe5a36acb0aeb69795949b0f683e43531
Chris K <chriskarasoulis@gmail.com>
2017-12-08 08:52:20 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.el.ts
+++ i18n/asteroid-calendar.el.ts
@@ -16,12 +16,12 @@
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Τίτλος</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>Εκδήλωση χωρίς τίτλο</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
@@ -31,12 +31,12 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Ατζέντα</translation>
     </message>
     <message id="id-month-page">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Μήνας</translation>
     </message>
 </context>
 </TS>
