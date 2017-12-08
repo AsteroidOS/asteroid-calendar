7813228e50a3c67429422a0ab310b0d00c74819f
Pedro Silva <pedrenriques@gmail.com>
2017-12-08 03:03:38 +0000

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.pt_PT.ts
+++ i18n/asteroid-calendar.pt_PT.ts
@@ -6,32 +6,32 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>Novo evento</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>Editar evento</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="97"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>Título</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="135"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>Evento sem nome</translation>
     </message>
     <message id="id-date-selection">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Select a date:</source>
-        <translation></translation>
+        <translation>Selecionar uma data:</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%1 Eventos em %2 %3/%4/%5</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
