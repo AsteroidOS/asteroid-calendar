56f9d12a15e8f426b1da1ae0bf2bc689c430af34
Joel Vinay Kumar <lavotivinay@gmail.com>
2018-02-01 11:03:45 +0000

    Translated using Weblate (Telugu)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.te.ts
+++ i18n/asteroid-calendar.te.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>కొత్త సమావేశం</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>సమావేశాన్ని మార్చు</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>టైటిల్</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>పేరులేని సమావేశం</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%1 సమావేశాలు %2 %3/%4/%5 న</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation></translation>
+        <translation>ఎజెండా</translation>
     </message>
     <message id="id-month-page">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation></translation>
+        <translation>నెల</translation>
     </message>
 </context>
 </TS>
