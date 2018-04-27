aa5865c993db37d412cb8c84ef0b3b387b09a07f
Hazem Mohamed Abdelmowla <hazem_boss@hotmail.com>
2018-04-27 14:13:58 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.ar_AR.ts
+++ i18n/asteroid-calendar.ar_AR.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>حدث جديد</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>تعديل حدث</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>عنوان</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>مناسبة بدون إسم</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%1 مناسبة في %2 %3/%4/%5</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation></translation>
+        <translation>المفكرة</translation>
     </message>
     <message id="id-month-page">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation></translation>
+        <translation>شهر</translation>
     </message>
 </context>
 </TS>
