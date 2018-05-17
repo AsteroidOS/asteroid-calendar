4504b1850bf8c63ed37e43016fa21c8c2a7a7cc9
ellan <ellan123200@gmail.com>
2018-05-17 14:17:46 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.zh_Hant.ts
+++ i18n/asteroid-calendar.zh_Hant.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>新增活動</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>編輯活動</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>標題</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>未命名活動</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%2 %3/%4/%5 有 %1 個活動</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation></translation>
+        <translation>行事曆</translation>
     </message>
     <message id="id-month-page">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation></translation>
+        <translation>月</translation>
     </message>
 </context>
 </TS>
