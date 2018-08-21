4b3c6aff90f58ee900b9671d09c4590384de34b8
mohamad farid <mfarid.ahamid@gmail.com>
2018-08-21 09:09:04 +0000

    Translated using Weblate (Malay)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.ms.ts
+++ i18n/asteroid-calendar.ms.ts
@@ -6,27 +6,27 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation></translation>
+        <translation>Acara Baru</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation></translation>
+        <translation>Sunting Acara</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation></translation>
+        <translation>Tajuk</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation></translation>
+        <translation>Acara Tidak Bertajuk</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation></translation>
+        <translation>%1 Acara pada %2 %3/%4/%5</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
@@ -36,7 +36,7 @@
     <message id="id-month-page">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation></translation>
+        <translation>Bulan</translation>
     </message>
 </context>
 </TS>
