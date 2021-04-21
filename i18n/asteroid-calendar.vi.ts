1832c416cbf2a51e903997686b808403a9ed63ec
bruh <quangtrung02hn16@gmail.com>
2021-04-21 15:50:17 +0200

    Added translation using Weblate (Vietnamese)

---
+++ i18n/asteroid-calendar.vi.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name></name>
+    <message id="id-new-event">
+        <location filename="../EventDialog.qml" line="43"/>
+        <source>New Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-edit-event">
+        <location filename="../EventDialog.qml" line="45"/>
+        <source>Edit Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-title-field">
+        <location filename="../EventDialog.qml" line="90"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-untitled-event">
+        <location filename="../EventDialog.qml" line="128"/>
+        <source>Untitled event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-events-recap">
+        <location filename="../main.qml" line="238"/>
+        <source>%1 Events on %2 %3/%4/%5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-calendar.desktop.h" line="6"/>
+        <source>Agenda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-month-page">
+        <location filename="../MonthSelector.qml" line="28"/>
+        <source>Month</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
