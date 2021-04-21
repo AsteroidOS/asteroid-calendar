27d9c4fc3d8985270679b7af9478853112b00874
bruh <quangtrung02hn16@gmail.com>
2021-04-21 13:52:16 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.vi.ts
+++ i18n/asteroid-calendar.vi.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Sự kiện mới</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Chỉnh sửa sự kiện</translation>
     </message>
     <message id="id-title-field">
         <location filename="../EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiêu đề</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>Sự kiện chưa có tiêu đề</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../main.qml" line="238"/>
         <source>%1 Events on %2 %3/%4/%5</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Sự kiện vào %2 %3/%4/%5</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Kế hoạch</translation>
     </message>
     <message id="id-month-page">
         <location filename="../MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Tháng</translation>
     </message>
 </context>
 </TS>
