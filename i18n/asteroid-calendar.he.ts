6e96a185d71e124eef2ad7856b5304e13d44ca1d
Yaron Shahrabani <sh.yaron@gmail.com>
2021-06-18 04:59:01 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.he.ts
+++ i18n/asteroid-calendar.he.ts
@@ -36,7 +36,7 @@
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>חודש</translation>
     </message>
 </context>
 </TS>
