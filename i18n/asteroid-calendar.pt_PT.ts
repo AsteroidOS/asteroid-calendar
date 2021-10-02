76e748be3901b3f8d0936db3fa8190905e8910f0
Nuno Araújo <araujonuno2000@gmail.com>
2021-10-02 18:14:58 +0000

    Translated using Weblate (Portuguese (Portugal))
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.pt_PT.ts
+++ i18n/asteroid-calendar.pt_PT.ts
@@ -36,7 +36,7 @@
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Mês</translation>
     </message>
 </context>
 </TS>
