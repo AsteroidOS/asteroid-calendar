dd10cbde905c24b5900fee3bfd73099526b44251
eLtMosen <koennecke@mosushi.de>
2022-02-21 16:37:58 +0100

    Adapt translations to removal of date from id-events-recap

--- i18n/asteroid-calendar.pt.ts
+++ i18n/asteroid-calendar.pt.ts
@@ -25,8 +25,8 @@
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
-        <source>%1 Events on %2 %3/%4/%5</source>
-        <translation>%1 evento(s) em %2 %3/%4/%5</translation>
+        <source>%1 Events on %2</source>
+        <translation>%1 evento(s) em %2</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
