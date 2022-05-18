acc536e543290117fc8e78469c356006a1e577f0
phlostically <phlostically@mailinator.com>
2022-05-18 09:38:03 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.eo.ts
+++ i18n/asteroid-calendar.eo.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../src/EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Nova evento</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../src/EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifi eventon</translation>
     </message>
     <message id="id-title-field">
         <location filename="../src/EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Titolo</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../src/EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>Sentitola evento</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 eventoj en %2</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Taskoj</translation>
     </message>
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Monato</translation>
     </message>
 </context>
 </TS>
