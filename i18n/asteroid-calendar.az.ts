e698ba1acc04a610b0127d875234ec32adba5c53
Corc <nizamismidov4@gmail.com>
2022-05-23 11:36:01 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.az.ts
+++ i18n/asteroid-calendar.az.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../src/EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeni Tədbir</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../src/EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Tədbiri Redaktə et</translation>
     </message>
     <message id="id-title-field">
         <location filename="../src/EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlıq</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../src/EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlıqsız tədbir</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Tarixində %2 Tədbirlər</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Gündəlik</translation>
     </message>
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Ay</translation>
     </message>
 </context>
 </TS>
