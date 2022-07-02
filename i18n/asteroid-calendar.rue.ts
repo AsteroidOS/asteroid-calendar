9d7a35aa9bf9e79c991e63cbb10ec38d332c0fda
Vondrosh Tenkač <shorksmesharik@gmail.com>
2022-07-02 07:02:19 +0000

    Translated using Weblate (Rusyn)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.rue.ts
+++ i18n/asteroid-calendar.rue.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../src/EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Новоє дїло</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../src/EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>Измінити дїло</translation>
     </message>
     <message id="id-title-field">
         <location filename="../src/EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Назывка</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../src/EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>Неназваноє дїло</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Дїло на %2</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Днёвый шор</translation>
     </message>
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>Місяц</translation>
     </message>
 </context>
 </TS>
