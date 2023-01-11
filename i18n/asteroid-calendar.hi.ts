e008ae27ac20a511937b2f91b89e6f4971023616
Xyngg <heyabhinav@protonmail.com>
2023-01-11 08:29:28 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.hi.ts
+++ i18n/asteroid-calendar.hi.ts
@@ -6,37 +6,37 @@
     <message id="id-new-event">
         <location filename="../src/EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>नयी घटना</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../src/EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>घटना संपादित करें</translation>
     </message>
     <message id="id-title-field">
         <location filename="../src/EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>शीर्षक</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../src/EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>शीर्षकहीन घटना</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 घटनाएं %2 पर</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>कार्यसूची</translation>
     </message>
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
         <source>Month</source>
-        <translation type="unfinished"></translation>
+        <translation>महीना</translation>
     </message>
 </context>
 </TS>
