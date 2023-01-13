c24fe39501f2bf8240bcaea5582352268d5bc150
Xyngg <heyabhinav@protonmail.com>
2023-01-13 01:29:04 +0100

    Added translation using Weblate (Punjabi)

---
+++ i18n/asteroid-calendar.pa.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name></name>
+    <message id="id-new-event">
+        <location filename="../src/EventDialog.qml" line="43"/>
+        <source>New Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-edit-event">
+        <location filename="../src/EventDialog.qml" line="45"/>
+        <source>Edit Event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-title-field">
+        <location filename="../src/EventDialog.qml" line="90"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-untitled-event">
+        <location filename="../src/EventDialog.qml" line="128"/>
+        <source>Untitled event</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-events-recap">
+        <location filename="../src/main.qml" line="238"/>
+        <source>%1 Events on %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-calendar.desktop.h" line="6"/>
+        <source>Agenda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-month-page">
+        <location filename="../src/MonthSelector.qml" line="28"/>
+        <source>Month</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
