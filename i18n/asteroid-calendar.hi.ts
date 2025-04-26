2bbb33579046e04bd0d91fa3ec863ae8185a63e5
Govind Gopal Yadav <govindgopalgaming@gmail.com>
2025-04-26 19:24:37 +0200

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.hi.ts
+++ i18n/asteroid-calendar.hi.ts
@@ -6,12 +6,12 @@
     <message id="id-new-event">
         <location filename="../src/EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation>नयी घटना</translation>
+        <translation>नया आयोजन/इवैंट</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../src/EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation>घटना संपादित करें</translation>
+        <translation>आयोजन/इवैंट संपादित करें</translation>
     </message>
     <message id="id-title-field">
         <location filename="../src/EventDialog.qml" line="90"/>
@@ -21,17 +21,17 @@
     <message id="id-untitled-event">
         <location filename="../src/EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation>शीर्षकहीन घटना</translation>
+        <translation>शीर्षक रहित आयोजन/इवैंट</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation>%1 घटनाएं %2 पर</translation>
+        <translation>%1 आयोजन/इवैंट %2 पर</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation>कार्यसूची</translation>
+        <translation>कार्यसूची/एजेंडा</translation>
     </message>
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
