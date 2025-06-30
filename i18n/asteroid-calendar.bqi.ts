a5ce367d96861606565f72ceaf870d15cd42755b
hosêyň abāspanā <hosseinabaspanah@gmail.com>
2025-06-30 06:48:45 +0200

    Translated using Weblate (Luri (Bakhtiari))
    Currently translated at 71.4% (5 of 7 strings)

--- i18n/asteroid-calendar.bqi.ts
+++ i18n/asteroid-calendar.bqi.ts
@@ -6,27 +6,27 @@
     <message id="id-new-event">
         <location filename="../src/EventDialog.qml" line="43"/>
         <source>New Event</source>
-        <translation type="unfinished"></translation>
+        <translation>تفاق نۊ</translation>
     </message>
     <message id="id-edit-event">
         <location filename="../src/EventDialog.qml" line="45"/>
         <source>Edit Event</source>
-        <translation type="unfinished"></translation>
+        <translation>آلشت تفاق</translation>
     </message>
     <message id="id-title-field">
         <location filename="../src/EventDialog.qml" line="90"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>عونوان</translation>
     </message>
     <message id="id-untitled-event">
         <location filename="../src/EventDialog.qml" line="128"/>
         <source>Untitled event</source>
-        <translation type="unfinished"></translation>
+        <translation>تفاق بؽ عونوان</translation>
     </message>
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>‫%1 تفاق من %2</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
