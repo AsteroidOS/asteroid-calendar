f3c6a437bfeff71f457777f6e8f1d6e5674608dc
Quentin PAGÈS <quentinantonin@free.fr>
2026-02-27 08:49:08 +0100

    Translated using Weblate (Occitan)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-calendar.oc.ts
+++ i18n/asteroid-calendar.oc.ts
@@ -26,12 +26,12 @@
     <message id="id-events-recap">
         <location filename="../src/main.qml" line="238"/>
         <source>%1 Events on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 eveniments lo %2</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-calendar.desktop.h" line="6"/>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation>Agenda</translation>
     </message>
     <message id="id-month-page">
         <location filename="../src/MonthSelector.qml" line="28"/>
